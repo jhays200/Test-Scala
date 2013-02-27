d1ec75b12653765899f626c0af39134d663cd975
EC2 user <ec2-user@domU-12-31-39-10-42-81.compute-1.internal>
2013-02-26 22:03:05 -0500

    Got a working httpClient with apache java library

--- build.sbt
+++ build.sbt
@@ -10,10 +10,16 @@ scalacOptions := Seq("-unchecked", "-deprecation", "-encoding", "utf8")
 
 resolvers ++= Seq(
 	"spray repo" at "http://repo.spray.io"
+	//"com.typesafe.akka" at "http://repo.typesafe.com/typesafe/releases/"
 )
 
 libraryDependencies ++= Seq(
-	"io.spray" % "spray-can" % "1.1-M7",
-	"io.spray" %%  "spray-json" % "1.2.3",
-	"com.typesafe.akka" %% "akka-actor" % "2.1.0"
+	"org.apache.httpcomponents" % "httpclient" % "4.2.3",
+	"io.spray" %%  "spray-json" % "1.2.3"
+	//"io.spray" % "spray-io" % "1.1-M7",
+	//"io.spray" % "spray-http" % "1.1-M7",
+	//"io.spray" % "spray-util" % "1.1-M7",
+	//"com.typesafe.akka" %% "akka-actor" % "2.1.1",
+	//"com.typesafe.akka" %% "akka-slf4j" % "2.1.1",
+	//"ch.qos.logback" % "logback-classic" % "1.0.9"
 )
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,9 +1,20 @@
-import spray.json._
+import org.apache.http.impl.client.DefaultHttpClient
+import org.apache.http.client.HttpClient
+import org.apache.http.client.methods._
+import org.apache.http.client.HttpClient
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
-object Test extends App {
-val source = """{"some":"JSON source" }"""
-val jsonAst = source.asJson
+object Test_Scala extends App {
+	var client = new DefaultHttpClient()
+	val request = new HttpGet("http://google.com/")
+	var response = client.execute(request)
 
-println(jsonAst.prettyPrint)
+	var rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
 
+	var line = rd.readLine()
+	while(line != null) {
+		println(line)
+		line = rd.readLine()
+	}
 }
